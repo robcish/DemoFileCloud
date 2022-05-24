c277d71e19e640ca18a19d10a7a2c7f33801aa61
Robert Różański <rrozanski@speednet.pl>
2022-05-24 23:00:23 +0200

    fonts, icons, images

--- app/src/main/java/com/example/demofilecloud/ui/theme/Type.kt
+++ app/src/main/java/com/example/demofilecloud/ui/theme/Type.kt
@@ -1,28 +1,19 @@
 package com.example.demofilecloud.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.demofilecloud.R
 
-// Set of Material typography styles to start with
+val interFont = FontFamily(
+    Font(R.font.inter_light, weight = FontWeight.Light), // 300
+    Font(R.font.inter_regular, weight = FontWeight.Normal), // 400
+    Font(R.font.inter_medium, weight = FontWeight.Medium), // 500
+    Font(R.font.inter_semibold, weight = FontWeight.SemiBold), // 600
+    Font(R.font.inter_bold, weight = FontWeight.Bold), // 700
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = interFont
 )

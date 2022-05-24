192037463ff0aa997a04c333965a661409c8f83d
Robert Różański <rrozanski@speednet.pl>
2022-05-24 16:13:36 +0200

    init App and api keys for giphy

---
+++ app/src/main/java/com/example/demofilecloud/App.kt
@@ -0,0 +1,42 @@
+package com.example.demofilecloud
+
+import android.app.Application
+import com.giphy.sdk.ui.Giphy
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Giphy.configure(this, Const.API_KEY)
+    }
+
+    companion object {
+
+        fun getRetrofitClient(): Retrofit {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .callTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
+
+            return Retrofit.Builder()
+                .client(client)
+                .baseUrl(Const.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/demofilecloud/Const.kt
@@ -0,0 +1,10 @@
+package com.example.demofilecloud
+
+class Const {
+
+    companion object {
+        const val API_KEY = "FLQWFFHxpff9kOp8DZSvoYFx4Zr8jxwi"
+        const val BASE_URL = "https://api.giphy.com/v1/"
+    }
+
+}

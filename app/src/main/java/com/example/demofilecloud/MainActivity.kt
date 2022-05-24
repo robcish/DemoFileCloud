aa51753d59d42bdfb8cdb18f7994a4226156c7fc
Robert Różański <rrozanski@speednet.pl>
2022-05-24 16:48:44 +0200

    navigation from main activity to cv activity

--- app/src/main/java/com/example/demofilecloud/MainActivity.kt
+++ app/src/main/java/com/example/demofilecloud/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.example.demofilecloud
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.demofilecloud.databinding.ActivityMainBinding
+import io.reactivex.functions.Action
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        init()
+    }
 
+    private fun init() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.setClickCV {
+            startActivity(Intent(this, CVActivity::class.java))
+        }
+    }
+
+    object RxActions {
+        @JvmStatic
+        fun runSafe(action: Action?) {
+            try {
+                action?.run()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
     }
 
 }
